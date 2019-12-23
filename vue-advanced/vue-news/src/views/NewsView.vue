6d832bd2a2c650a28ed3ae7794317bf7b257030b
donghoonlee94 <94donghoon.lee@gmail.com>
2019-12-24 00:02:54 +0900

    router,axios 실습

---
+++ vue-advanced/vue-news/src/views/NewsView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div >
+    <p v-for="user in users" :key="user.id">
+          {{user}}
+    </p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users : [],
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function(response) {
+        vm.users = response.data
+      })
+      .catch(function(error){
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
